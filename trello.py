6306aa304efd6e77f74c5da526651523eaae786a
Richard Kolkovich <richard@sigil.org>
2012-01-04 00:19:08 -0700

    Add method to add a card

--- trello.py
+++ trello.py
@@ -56,3 +56,56 @@ def list_boards():
 
 	json_obj = json.loads(content)
 	return json_obj.boards
+
+def add_card(board_id, name):
+	"""Adds a card to the first list in the given board
+	
+	:board_id: @todo
+	:name: @todo
+	:returns: @todo
+	"""
+
+	if not cookie:
+		raise AuthenticationRequired()
+
+	headers = {'Cookie': cookie, 'Accept': 'application/json'}
+	response, content = client.request(
+			'https://trello.com/data/board/'+board_id+'/current',
+			'GET',
+			headers = headers,
+			)
+
+	# TODO: error checking
+
+	json_obj = json.loads(content)
+
+	# get first list
+	list_id = ""
+	for board in json_obj.boards:
+		if board.lists:
+			list_id = board.lists[0]._id
+	
+	# TODO: ensure list was found
+	request = {
+			'token': cookie, #TODO: not right - extract token
+			'method': 'create',
+			'data': {
+				'attrs': {
+					'name': name,
+					'pos': 65536,
+					'closed': false,
+					'idBoard': board_id,
+					"idList": list_id,
+					},
+				'idParents': [ board_id, list_id ],
+				}
+
+	response, content = client.request(
+			'https://trello.com/api/card',
+			'POST',
+			headers = headers,
+			body = json.dumps(request),
+			)
+
+	# TODO: error checking
+
