f06c7813663dc9ac4bf63601574617acf5d7324d
Richard Kolkovich <richard@sigil.org>
2012-01-06 22:44:54 -0700

    Add some more test cases

--- tests.py
+++ tests.py
@@ -1,19 +1,32 @@
 from models import AuthenticationError,AuthenticationRequired
-import trello
+from trello import Trello
 import unittest
 import os
 
-class TestTrello(unittest.TestCase):
+class BoardTestCase(unittest.TestCase):
 
-	def test_login(self):
-		username = os.environ['TRELLO_TEST_USER']
-		password = os.environ['TRELLO_TEST_PASS']
-		try:
-			trello.login(username, password)
-		except AuthenticationError:
-			self.fail("Could not authenticate")
-		except Exception as e:
-			self.fail("Unknown error: "+str(e))
+	def setUp(self):
+		self._trello = Trello(os.environ['TRELLO_TEST_USER'], os.environ['TRELLO_TEST_PASS'])
+
+	def test01_list_boards(self):
+		print "list boards"
+		self.assertEquals(
+				len(self._trello.list_boards()),
+				int(os.environ['TRELLO_TEST_BOARD_COUNT']))
+
+	def test02_board_attrs(self):
+		print "board attrs"
+		boards = self._trello.list_boards()
+		for b in boards:
+			self.assertIsNotNone(b['_id'], msg="_id not provided")
+			self.assertIsNotNone(b['name'], msg="name not provided")
+			self.assertIsNotNone(b['closed'], msg="closed not provided")
+
+class CardTestCase(unittest.TestCase):
+	def setUp(self):
+		self._trello = Trello(os.environ['TRELLO_TEST_USER'], os.environ['TRELLO_TEST_PASS'])
+
+
 
 if __name__ == "__main__":
 	unittest.main()
