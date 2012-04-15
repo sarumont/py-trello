39011e735a6fb30943ce73345760a9a7b9d75da0
Richard Kolkovich <richard@sigil.org>
2012-04-15 16:51:23 -0600

    update metadata some more

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "py-trello",
-	version = "0.1.1",
+	version = "0.1.2",
 
 	description = 'Python wrapper around the Trello API',
 	long_description = open('README.rst').read(),
