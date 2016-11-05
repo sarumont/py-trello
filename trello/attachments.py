8c666329bb0736d79ce23611460b038bf708766c
Sergey G <s.gaynetdinov@gmail.com>
2016-11-05 13:09:59 +0500

    Refactoring params

--- trello/attachments.py
+++ trello/attachments.py
@@ -5,14 +5,14 @@ class Attachments(object):
     """
     https://developers.trello.com/advanced-reference/card#get-1-cards-card-id-or-shortlink-attachments
     """
-    def __init__(self, id, bytes, date, edgeColor, idMember, isUpload, mimeType, name, previews, url):
+    def __init__(self, id, bytes, date, edge_color, idMember, is_upload, mime_type, name, previews, url):
         self.id = id
         self.bytes = bytes
         self.date = date
-        self.edge_color = edgeColor
+        self.edge_color = edge_color
         self.idMember = idMember
-        self.is_upload = isUpload
-        self.mime_type = mimeType
+        self.is_upload = is_upload
+        self.mime_type = mime_type
         self.name = name
         self.previews = previews
         self.url = url
@@ -22,27 +22,27 @@ class Attachments(object):
         id = json_obj.get("id")
         bytes = json_obj.get("bytes")
         date = json_obj.get("date")
-        edgeColor = json_obj.get("edgeColor")
+        edge_color = json_obj.get("edgeColor")
         idMember = json_obj.get("idMember")
-        isUpload = json_obj.get("isUpload")
-        mimeType = json_obj.get("mimeType")
+        is_upload = json_obj.get("isUpload")
+        mime_type = json_obj.get("mimeType")
         name = json_obj.get("name")
         previews = [AttachmentsPreview.from_json(preview_json) for preview_json in json_obj.get("previews")]
         url = json_obj.get("url")
 
-        return Attachments(id, bytes, date, edgeColor, idMember, isUpload, mimeType, name, previews, url)
+        return Attachments(id, bytes, date, edge_color, idMember, is_upload, mime_type, name, previews, url)
 
     def __repr__(self):
         return u"<Attachments {0}>".format(self.name)
 
 
 class AttachmentsPreview(object):
-    def __init__(self, bytes, url, width, height, scaled):
+    def __init__(self, bytes, url, width, height, is_scaled):
         self.bytes = bytes
         self.url = url
         self.width = width
         self.height = height
-        self.is_scaled = scaled
+        self.is_scaled = is_scaled
 
     @staticmethod
     def from_json(json_obj):
@@ -50,9 +50,9 @@ class AttachmentsPreview(object):
         url = json_obj.get("url")
         width = json_obj.get("width")
         height = json_obj.get("height")
-        scaled = json_obj.get("scaled")
+        is_scaled = json_obj.get("scaled")
 
-        return AttachmentsPreview(bytes, url, width, height, scaled)
+        return AttachmentsPreview(bytes, url, width, height, is_scaled)
 
     def __repr__(self):
         return u"<Attachments Preview {0}x{1}".format(self.width, self.height)
