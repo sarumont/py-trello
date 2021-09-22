a66b525e7fdc8d4122bb9aba5b00fa9341e54818
alexander-minchin <83529885+alexander-minchin@users.noreply.github.com>
2021-09-22 17:14:21 +0930

    fix: incorrect string format index
    The second index in the string format was changed from 0 to 1.

--- trello/board.py
+++ trello/board.py
@@ -291,7 +291,7 @@ class Board(TrelloBase):
 		"""
 
 		json_obj = self.client.fetch_json(
-				'customFields/{0}/options/{0}'.format(custom_field_definition_id,option_id),
+				'customFields/{0}/options/{1}'.format(custom_field_definition_id,option_id),
 				http_method='GET',
 				)
 		# if values_only:
@@ -308,7 +308,7 @@ class Board(TrelloBase):
 		"""
 
 		json_obj = self.client.fetch_json(
-				'customFields/{0}/options/{0}'.format(custom_field_definition_id,option_id),
+				'customFields/{0}/options/{1}'.format(custom_field_definition_id,option_id),
 				http_method='DELETE',
 				)
 		# if values_only:
